cfa86ce23d65673953be509925bc03b29fe31f0e
Daniel Mercier <daniel.mercier@u-psud.fr>
2016-03-06 01:15:24 +0000

    Added tests for for loop

---
+++ compilo/tests/for/t00.ml
@@ -0,0 +1,4 @@
+print_int 1234; print_int 5678; print_int 910; print_newline ();;
+for x = 1 to 10 do
+    print_int x
+done;;
---
+++ compilo/tests/for/t01.ml
@@ -0,0 +1,6 @@
+let from = 2;;
+let too = 5;;
+
+for i = from to too do 
+    print_int i 
+done;;
